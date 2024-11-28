539fe5bf941cbe3abd0abd2b5516dfccf15eccc3
Buquoy <2590204635@qq.com>
2024-11-28 09:35:53 +0800

    add building merging

---
+++ scripts/MergeBuildings.py
@@ -0,0 +1,210 @@
+import json
+import os
+import re
+
+# Data structure:
+# {
+#     "state_name": {
+#         "tag": {
+#             "create_building": [
+#                 {
+#                     "building": "building_name",
+#                     "add_ownership": [
+#                         {
+#                             "building": [
+#                                 {
+#                                     "type": "ownership",
+#                                     "country": "country_tag"
+#                                     "levels": "levels"
+#                                     "region": "region"
+#                                 }
+#                             ]
+#                             "country": [
+#                                 {
+#                                     "country": "country_tag"
+#                                     "levels": "levels"
+#                             ]
+#                             "company": [
+#                                 {
+#                                     "type": "company_tag"
+#                                     "country": "country_tag"
+#                                     "levels": "levels"
+#                                 }
+#                         }
+#                     ]
+#                 }
+#             ]
+# # other keys are ignored
+#         }
+
+def merge(this, other): # this, other are "state_name" values
+    '''Merge two state buildings.
+    '''
+    for tag in other.keys():
+        if isinstance(other[tag], list):
+            continue
+        if tag in this.keys():
+            if isinstance(this[tag], list):
+                this[tag] = other[tag]
+                continue
+            for other_building in other[tag]["create_building"]:
+                found = False
+                for this_building in this[tag]["create_building"]:
+                    if this_building["building"] == other_building["building"]:
+                        found = True
+                        if "building" in other_building["add_ownership"][0].keys():
+                            if "building" in this_building["add_ownership"][0].keys():
+                                for other_ownership in other_building["add_ownership"][0]["building"]:
+                                    found_ownership = False
+                                    for this_ownership in this_building["add_ownership"][0]["building"]:
+                                        # if "type", "country", "region" are the same, merge "levels"
+                                        if this_ownership["type"] == other_ownership["type"] and this_ownership["country"] == other_ownership["country"] and this_ownership["region"] == other_ownership["region"]:
+                                            found_ownership = True
+                                            this_ownership["levels"] = int(this_ownership["levels"]) + int(other_ownership["levels"])
+                                            break
+                                    if not found_ownership:
+                                        this_building["add_ownership"][0]["building"].append(other_ownership)
+                            else:
+                                this_building["add_ownership"][0]["building"] = other_building["add_ownership"][0]["building"]
+                        if "country" in other_building["add_ownership"][0].keys():
+                            if "country" in this_building["add_ownership"][0].keys():
+                                for other_ownership in other_building["add_ownership"][0]["country"]:
+                                    found_ownership = False
+                                    for this_ownership in this_building["add_ownership"][0]["country"]:
+                                        # if "country" is the same, merge "levels"
+                                        if this_ownership["country"] == other_ownership["country"]:
+                                            found_ownership = True
+                                            this_ownership["levels"] = int(this_ownership["levels"]) + int(other_ownership["levels"])
+                                            break
+                                    if not found_ownership:
+                                        this_building["add_ownership"][0]["country"].append(other_ownership)
+                            else:
+                                this_building["add_ownership"][0]["country"] = other_building["add_ownership"][0]["country"]
+                        if "company" in other_building["add_ownership"][0].keys():
+                            if "company" in this_building["add_ownership"][0].keys():
+                                for other_ownership in other_building["add_ownership"][0]["company"]:
+                                    found_ownership = False
+                                    for this_ownership in this_building["add_ownership"][0]["company"]:
+                                        # if "type", "country" are the same, merge "levels"
+                                        if this_ownership["type"] == other_ownership["type"] and this_ownership["country"] == other_ownership["country"]:
+                                            found_ownership = True
+                                            this_ownership["levels"] = int(this_ownership["levels"]) + int(other_ownership["levels"])
+                                            break
+                                    if not found_ownership:
+                                        this_building["add_ownership"][0]["company"].append(other_ownership)
+                        break
+                if not found:
+                    this[tag]["create_building"].append(other_building)
+        else:
+            this[tag] = other[tag]
+
+def export(this, name):
+    '''Export the state object to a string
+    '''
+    if name == "if":
+        return ""
+    print('Exporting', name)
+    state_str = f'    {name} = {{\n'
+    for tag in this.keys():
+        state_str += f'        {tag} = {{\n'
+        if isinstance(this[tag], dict):
+            for building in this[tag]["create_building"]:
+                state_str += f'            create_building = {{\n'
+                state_str += f'                building = {building["building"]}\n'
+                if "add_ownership" not in building.keys(): # is monument, only has key "building" & "level"
+                    state_str += f'                level = {building["level"]}\n'
+                    state_str += f'            }}\n'
+                    continue
+                state_str += f'                add_ownership = {{\n'
+                # print(name, building["building"], building.keys())
+                for owner in building["add_ownership"]:
+                    if "building" in owner.keys():
+                        state_str += f'                    building = {{\n'
+                        for ownership in owner["building"]:
+                            state_str += f'                        type = {ownership["type"]}\n'
+                            state_str += f'                        country = {ownership["country"]}\n'
+                            state_str += f'                        levels = {ownership["levels"]}\n'
+                            state_str += f'                        region = {ownership["region"]}\n'
+                        state_str += f'                    }}\n'
+                    if "country" in owner.keys():
+                        state_str += f'                    country = {{\n'
+                        for ownership in owner["country"]:
+                            state_str += f'                        country = {ownership["country"]}\n'
+                            state_str += f'                        levels = {ownership["levels"]}\n'
+                        state_str += f'                    }}\n'
+                    if "company" in owner.keys():
+                        state_str += f'                    company = {{\n'
+                        for ownership in owner["company"]:
+                            state_str += f'                        type = {ownership["type"]}\n'
+                            state_str += f'                        country = {ownership["country"]}\n'
+                            state_str += f'                        levels = {ownership["levels"]}\n'
+                        state_str += f'                    }}\n'
+                state_str += f'                }}\n'
+                # Building cash reserve
+                if "reserves" in building.keys():
+                    state_str += f'                reserves = {building["reserves"]}\n'
+                # Building production methodss
+                if "activate_production_methods" in building.keys():
+                    state_str += f'                activate_production_methods = {{\n'
+                    if isinstance(building["activate_production_methods"], str):
+                        state_str += f'                    {building["activate_production_methods"]}\n'
+                    else:
+                        for method in building["activate_production_methods"]:
+                            state_str += f'                    {method}\n'
+                    state_str += f'                }}\n'
+                state_str += f'            }}\n'
+        state_str += f'        }}\n'
+    state_str += f'    }}\n'
+
+    return state_str
+
+
+# Destination directory
+buildings_file = 'StateMergingDevToolkit\\buildings\\05_north_america.json'
+
+# Read states from json
+with open(buildings_file, 'r', encoding='utf-8') as file:
+    building_dict = json.load(file)
+
+# Merge states
+merge_file = 'StateMergingDevToolkit\\merge_states.json'
+with open(merge_file, 'r', encoding='utf-8') as file:
+    merge_dict = json.load(file)
+
+# Merge building ownerships
+for diner, food_list in merge_dict.items():
+    for state_name in building_dict.keys():
+        if state_name == "if":
+            continue
+        for tag in building_dict[state_name].keys():
+            if isinstance(building_dict[state_name][tag], list):
+                continue
+            for building in building_dict[state_name][tag]["create_building"]:
+                if "add_ownership" not in building.keys():
+                    continue
+                if "building" not in building["add_ownership"][0].keys():
+                    continue
+                for owner in building["add_ownership"][0]["building"]:
+                    # Remove '\"' from owner["region"]
+                    region = owner["region"].replace('\"', '')
+                    if region in food_list:
+                        owner["region"] = '\"'+diner+'\"'
+
+
+# Merge buildings
+for diner, food_list in merge_dict.items():
+    for food in food_list:
+        if ("s:"+food) in building_dict.keys():
+            merge(building_dict[("s:"+diner)], building_dict[("s:"+food)])
+            print(f'Merged {food} into {diner}')
+            building_dict.pop("s:"+food)
+
+# Export states
+output_file = 'StateMergingDevToolkit\\buildings\\05_north_america.txt'
+with open(output_file, 'w', encoding='utf-8') as file:
+    file.write('BUILDINGS = {\n')
+    for state_name in building_dict.keys():
+        file.write(export(building_dict[state_name], state_name))
+    file.write('}\n')
+
+print("Don't forget to manually add the 'if dlc' buildings to the output file!")
---
+++ scripts/mod_state_script_buildings.py
@@ -0,0 +1,141 @@
+from mod_state import ModState
+import json
+import os
+
+base_game_dir = {
+    # "Building Groups": r"G:/SteamLibrary/steamapps/common/Victoria 3/game/common/building_groups",
+    # "Buildings": r"G:/SteamLibrary/steamapps/common/Victoria 3/game/common/buildings",
+    # "Technologies": r"G:/SteamLibrary/steamapps/common/Victoria 3/game/common/technology/technologies",
+    # "PM Groups": r"G:/SteamLibrary/steamapps/common/Victoria 3/game/common/production_method_groups",
+    # "PMs": r"G:/SteamLibrary/steamapps/common/Victoria 3/game/common/production_methods",
+    # "Ideologies": r"G:/SteamLibrary/steamapps/common/Victoria 3/game/common/ideologies",
+    # "States": r"C:/Users/ASUS/Documents/Codes/Victoria-3-files/game/map_data/state_regions",
+    # "Pops": r"C:/Users/ASUS/Documents/Codes/Vic3/mod",
+    "Buildings": r"C:/Users/ASUS/Documents/Codes/StateMergingDevToolkit/mod",
+}
+
+mod_dir = {
+    # "Building Groups": r"F:/Libraries/Documents/Paradox Interactive/Victoria 3/mod/Production Methods/common/building_groups",
+    # "Buildings": r"F:/Libraries/Documents/Paradox Interactive/Victoria 3/mod/Production Methods/common/buildings",
+    # "Technologies": r"F:/Libraries/Documents/Paradox Interactive/Victoria 3/mod/Production Methods/common/technology/technologies",
+    # "PM Groups": r"F:/Libraries/Documents/Paradox Interactive/Victoria 3/mod/Production Methods/common/production_method_groups",
+    # "PMs": r"F:/Libraries/Documents/Paradox Interactive/Victoria 3/mod/Production Methods/common/production_methods",
+    # "Ideologies": r"F:/Libraries/Documents/Paradox Interactive/Victoria 3/mod/Production Methods/common/ideologies",
+    # "States": r"C:/Users/ASUS/Documents/Codes/Vic3/mod",
+    # "Pops": r"C:/Users/ASUS/Documents/Codes/Vic3/output",
+    "Buildings": r"C:/Users/ASUS/Documents/Codes/StateMergingDevToolkit/output",
+}
+
+mod_state = ModState(base_game_dir, mod_dir)
+
+buildings = mod_state.get_data("Buildings")
+
+idleElements = [None, "", '=', '\"=\"']
+
+# Remove the first element of each element in the dictionary recursively despite it's a tuple
+def remove_idle_element(dic):
+    if isinstance(dic, tuple):
+        dic = remove_idle_element(list(dic))
+    if isinstance(dic, list):
+        for i in range(len(dic) - 1, -1, -1):
+            if dic[i] in idleElements:
+                dic.pop(i)
+            else:
+                dic[i] = remove_idle_element(dic[i])
+        if len(dic) == 1:
+            if isinstance(dic[0], dict):
+                if "building" in dic[0].keys() or "country" in dic[0].keys() or "company" in dic[0].keys():
+                    return dic
+            return dic[0]
+    elif isinstance(dic, dict):
+        for key, value in dic.items():
+            if value in idleElements:
+                dic.pop(key)
+            else:
+                dic[key] = remove_idle_element(value)
+        return dic
+    return dic
+
+buildings = remove_idle_element(buildings)
+
+# for pop_id, (_, pop_data) in pops.items():
+#     if "lawgroup_rights_of_women" in pop_data.keys():
+#         laws = pop_data["lawgroup_rights_of_women"][1].keys()
+#         if "law_protected_class" not in laws:
+#             print(pop_id, laws)
+
+
+# Write data to json
+with open(mod_dir["Buildings"]+"/all_buildings.json", "w", encoding="utf-8") as file:
+    json.dump(buildings["BUILDINGS"], file, ensure_ascii=False, indent=4)
+
+"""
+bonuses = {}
+counts = {}
+eras = [
+    "era_1",
+    "era_2",
+    "era_3",
+    "era_4",
+    "era_5",
+    "era_6",
+    "era_7",
+    "era_8",
+    "era_9",
+    "era_10",
+    "era_11",
+]
+
+for era in eras:
+    for category in ["production", "military", "society"]:
+        bonuses[(category, era)] = 0
+    counts[era] = 0
+
+tech = mod_state.get_data("Technologies")
+for tech_id, (_, tech_data) in tech.items():
+    _, category = tech_data["category"]
+    _, era = tech_data["era"]
+    if era not in eras:
+        continue
+    counts[era] += 1
+    if "modifier" not in tech_data.keys():
+        continue
+    _, modifier = tech_data["modifier"]
+    if len(modifier) == 0 or "country_weekly_innovation_max_add" not in modifier.keys():
+        continue
+    _, bonus = modifier["country_weekly_innovation_max_add"]
+    bonuses[(category, era)] += int(bonus)
+
+years_per_era = [40, 40, 40, 40, 30, 30, 20, 20, 20, 20, 20]
+weeks_per_era = [52 * year for year in years_per_era]
+research_speeds = {}
+for era in eras:
+    research_speeds[era] = 0
+for era in eras:
+    for category in ["production", "military", "society"]:
+        research_speeds[era] += bonuses[(category, era)]
+
+cumulative_research_speed = 200
+for era_num in range(1, len(eras)):
+    era = eras[era_num]
+    end_speed, start_speed = (
+        research_speeds[era] + cumulative_research_speed,
+        cumulative_research_speed,
+    )
+    cumulative_research_speed = end_speed
+    avg_research_speed = (end_speed + start_speed) / 2
+    cost_per_tech = avg_research_speed * weeks_per_era[era_num] / counts[era]
+    print(
+        era,
+        "cost per tech: ",
+        cost_per_tech,
+        " research speed: ",
+        avg_research_speed,
+        " tech count: ",
+        counts[era],
+    )
+"""
+
+# Updating and writing back to a file
+# buildings_data['new_building'] = {...}
+# mod_state.update_and_write_file("Buildings", "/path/to/mod/buildings/updated_building.txt")
